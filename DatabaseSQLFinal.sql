d4863666b5a8473dff5f3d3a1edb9ae6086d9a60
Chaseis4344 <73896908+Chaseis4344@users.noreply.github.com>
2022-04-15 12:56:29 -0400

    Add files via upload

--- DatabaseSQLFinal.sql
+++ DatabaseSQLFinal.sql
@@ -1,5 +1,6 @@
+
 REM   Script: Database_Proj_FINAL
-REM   aaaaa
+REM   Team Names: Kristen Linares, Michael Chase Townsend, David Carbajal
 
 create table REQUEST 
 ( 
@@ -8,22 +9,21 @@ create table REQUEST
     Customer_ID     number, 
     Depart_ID       number, 
     Selection_ID    number, 
-    Food            varchar(20), 
+    Food            varchar(50), 
     Price           number, 
     Request_Date    date, 
     Request_Time    timestamp, 
         constraint pk_req_ID primary key (Request_ID) 
-       --constraint fk_Customer_ID_req   foreign key (Customer_ID)   references CUSTOMER(Customer_ID), 
-       -- constraint fk_Depart_ID_req     foreign key (Depart_ID)     references REGIONAL_DEPARTMENT(Depart_ID), 
-       --constraint fk_Selection_ID_req  foreign key (Selection_ID)  references SELECTION (Selection_ID) 
+       
 );
 
 create table REGIONAL_DEPARTMENT(   
-    Depart_ID       number,   
+    Depart_ID       number ,   
     Depart_Area     varchar2(12),   
     Depart_Size     number,   
     Depart_Phone    char(17),   
-        constraint pk_Depart_ID primary key (Depart_ID)   
+        constraint pk_Depart_ID primary key (Depart_ID)   ,
+        constraint Depart_ID_Range check (Depart_ID >=50000000000001 and Depart_ID<= 99999999999999 )
    
 );
 
@@ -73,8 +73,8 @@ create table DRIVER(
 create table MENU(  
     Menu_ID         number,  
     Restaurant_ID   number,  
-    Items           char(15),  
-    Prices          number,  
+    Items           varchar(100),  
+    Prices         varchar2(200),  
         constraint pk_Menu_ID primary key (Menu_ID),  
         constraint fk_Restaurant_ID_Menu foreign key (Restaurant_ID) references RESTAURANT(Restaurant_ID)   
       
@@ -92,68 +92,4 @@ create table SELECTION(
  
 );
 
-alter table "REQUEST" add constraint fk_Customer_ID_req   foreign key (Customer_ID)   references CUSTOMER(Customer_ID);
-
-alter table "REQUEST" add constraint fk_Depart_ID_req     foreign key (Depart_ID)     references REGIONAL_DEPARTMENT(Depart_ID);
-
-alter table "REQUEST" add constraint fk_Selection_ID_req  foreign key (Selection_ID)  references SELECTION (Selection_ID) ;
-
-alter table "CUSTOMER" add constraint fk_driveID_cust   foreign key (Driver_ID) references DRIVER(Driver_ID);
-
- insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12893, 70511, 23492, ’JG9SWUFRGJSGGTSBE’, ‘Blue’, ‘Daimler AG’, ‘Mercedes-Benz’);
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12894, 70512, 23493, ‘RE9HDUFKLHJAWAFRC’, White’, ‘Stelantis’, ‘Jeep’);
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12895, 70513, 23494, ‘TY9JFSXMNGDKLTYSHR’, ’Red’, ‘Hondar Motor Co. ’, ‘Honda’);
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12896, 70514, 23495, ‘YR9HKPXHRWURWHWXS’, ‘Red’, ‘Ford Motor Co.’, ‘Ford’);
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24500, 5936, 4588320794758, ‘Nicholas’, ‘Walters’, +01(863)-431-2463);
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24501, 5937, 4588320794759, ‘Tye’, ‘Warwick’, +01(863)-543-7122);
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24502, 5938, 4588320794760, ‘Lori’, ‘Bonner’, +01(321)-427-8765);
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24502, 5939, 4588320794761, ‘Marshal’, ‘Tobias’, +01(954)-652-6363);
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24503, 5940, 4588320794762, ‘Alissa’, ‘Sumner’, +01(780)-421-8776);
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12892, 3323133, 7730533, 'JB9UFEWDYSBYMEUXR', 'Red', 'Tesla Inc.', 'Tesla');
-
- insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12893, 70511, 23492, 'JG9SWUFRGJSGGTSBE', 'Blue', 'Daimler AG', 'Mercedes-Benz');
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12894, 70512, 23493, 'RE9HDUFKLHJAWAFRC', 'White', 'Stelantis', 'Jeep');
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12895, 70513, 23494, 'TY9JFSXMNGDKLTYSHR', 'Red', 'Hondar Motor Co. ', 'Honda');
-
-insert into DRIVER (Driver_ID, Request_ID, Depart_ID, Driver_VIN, Driver_Color, Driver_Make, Driver_Model) 
-values (12896, 70514, 23495, 'YR9HKPXHRWURWHWXS', 'Red', 'Ford Motor Co.', 'Ford');
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24500, 5936, 4588320794758, 'Nicholas', 'Walters', '+01(863)-431-2463');
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24501, 5937, 4588320794759, 'Tye', 'Warwick', '+01(863)-543-7122');
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24502, 5938, 4588320794760, 'Lori', 'Bonner', '+01(321)-427-8765');
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24502, 5939, 4588320794761, 'Marshal', 'Tobias', '+01(954)-652-6363');
-
-insert into CUSTOMER (Customer_ID, Driver_ID, Request_ID, Customer_FName, Customer_LName, Customer_Phone) 
-values (24503, 5940, 4588320794762, 'Alissa', 'Sumner', '+01(780)-421-8776');
 
