ef3e899b5a8a20cea1aed042a6fba0eb274b5a60
Chaseis4344 <73896908+Chaseis4344@users.noreply.github.com>
2022-04-15 15:12:24 -0400

    Update Insert statements SQL.sql

--- Insert statements SQL.sql
+++ Insert statements SQL.sql
@@ -16,85 +16,85 @@ values (500000000000014 , 'Tallahassee', 20, '+01(863)-541-7142');
 
 
 insert into RESTAURANT 
-values(700000000 , '1800 FL-559, Polk City, FL 33868' , '36.98 40.64',  '01(780)-555-8776' );
+values(1 , '1800 FL-559, Polk City, FL 33868' , '36.98 40.64',  '01(780)-555-8776' );
 
 
 insert into RESTAURANT 
-values(700000001,'1115 N Berkley Rd, Auburndale, FL 33823',  '42.75,33.22' , '+01(780)-981-7432');
+values(2,'1115 N Berkley Rd, Auburndale, FL 33823',  '42.75,33.22' , '+01(780)-981-7432');
 
 
 insert into RESTAURANT 
-values(700000002, '3427 US Hwy 98 N, Lakeland, FL 33809', '40.44, 20.55' , '+01(863)-539-7742' );
+values(3, '3427 US Hwy 98 N, Lakeland, FL 33809', '40.44, 20.55' , '+01(863)-539-7742' );
 
 
 insert into RESTAURANT 
-values(700000003, '330 Denton Ave, Auburndale, FL 33823',   '20.11 , 11.23' , '+01(863)-221-8646');
+values(4, '330 Denton Ave, Auburndale, FL 33823',   '20.11 , 11.23' , '+01(863)-221-8646');
 
 
 insert into RESTAURANT 
-values(700000004, '221 Commonwealth Ave N, Polk City, FL 33868' , '23.55, 33.64' , '+01(954)-644-6497');
+values(5, '221 Commonwealth Ave N, Polk City, FL 33868' , '23.55, 33.64' , '+01(954)-644-6497');
 
 
 
 
 insert into menu
-values (80000000001, 700000000, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
+values (1, 1, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
         '6.99, 5.99, 4.99, 8.99, 8.99');
 
 insert into menu
-values (80000000002, 700000001, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
+values (2, 3, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
         '6.99, 5.99, 4.99, 8.99, 8.99');
 
 insert into menu
-values (80000000003, 700000002, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets',
+values (3, 3, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets',
         '6.99, 5.99, 4.99, 8.99, 8.99');
 
 insert into menu
-values (80000000004, 700000003, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
+values (4, 4, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
         '6.99, 5.99, 4.99, 8.99, 8.99');
 
 insert into menu
-values (80000000005, 700000004, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
+values (5, 5, 'hamburger and fries, Grilled Chicken Sandwich, Nachos, Chicken nuggets', 
         '6.99, 5.99, 4.99, 8.99, 8.99');
 
 
 
 
 insert into REQUEST
-values (20000000002018, 3000000, 500000000000010, 400000000000100  , 'Hamburger and fries',7.99 , '20-MAR-2022', '03-20-2022 12:22:22');
+values (1, 1,1,1  , 'Hamburger and fries',7.99 , '20-MAR-2022', '03-20-2022 12:22:22');
 
 insert into REQUEST
-values (20000000002019, 3000001, 500000000000011 , 400000000000101, 'Grilled Chicken Sandwich', 5.99, '21-MAR-2022', '03-21-2022 13:09:23');
+values (2, 2, 2 ,2 , 'Grilled Chicken Sandwich', 5.99, '21-MAR-2022', '03-21-2022 13:09:23');
 
 insert into REQUEST
-values (20000000002020, 3000002, 500000000000012, 400000000000102, 'Spicy Chicken Sandwich', 4.99, '01-FEB-2022', '02-01-2022 20:00:33');
+values (3, 3, 3, 3, 'Spicy Chicken Sandwich', 4.99, '01-FEB-2022', '02-01-2022 20:00:33');
 
 insert into REQUEST
-values (20000000002021, 3000003, 500000000000013 , 400000000000103, 'Nachos', 8.99, '01-JAN-2022', '01-01-2022 11:24:43');
+values (4, 4, 4 , 4, 'Nachos', 8.99, '01-JAN-2022', '01-01-2022 11:24:43');
 
 insert into REQUEST
-values (20000000002022, 3000004, 500000000000014, 400000000000104, 'Chicken nuggets', 8.99, '02-APR-2022',  '04-02-2022 14:01:11' );
+values (5, 5, 5, 5, 'Chicken nuggets', 8.99, '02-APR-2022',  '04-02-2022 14:01:11' );
 
 
 
 insert into CUSTOMER 
-values (3000000, 100100, 20000000003018, 'Nicholas', 'Walters', '+01(863)-431-2463');
+values (1, 1, 1, 'Nicholas', 'Walters', '+01(863)-431-2463');
 
 
 insert into CUSTOMER 
-values (3000001, 100101, 20000000003019, 'Tye', 'Warwick', '+01(863)-543-7122');
+values (2, 2, 2, 'Tye', 'Warwick', '+01(863)-543-7122');
 
 
 insert into CUSTOMER 
-values (3000002, 100102, 20000000003021, 'Lori', 'Bonner', '+01(321)-427-8765');
+values (3, 3, 3, 'Lori', 'Bonner', '+01(321)-427-8765');
 
 
 insert into CUSTOMER 
-values (3000003, 100103, 20000000003021, 'Marshal', 'Tobias', '+01(954)-652-6363');
+values (4, 4, 4, 'Marshal', 'Tobias', '+01(954)-652-6363');
 
 
 insert into CUSTOMER 
-values (3000004, 100104 , 20000000003020, 'Alissa', 'Sumner', '+01(780)-421-8776');
+values (5, 5 , 5, 'Alissa', 'Sumner', '+01(780)-421-8776');
 
 
 
@@ -104,19 +104,19 @@ values (3000004, 100104 , 20000000003020, 'Alissa', 'Sumner', '+01(780)-421-8776
 
 
 insert into BCUSTOMER 
-values (3000000, 20000000003018, 'Jude Breckenridge', '+01(239)-543-4122' );
+values (1, 1, 'Jude Breckenridge', '+01(239)-543-4122' );
 
 insert into BCUSTOMER 
-values (3000001, 20000000003019, 'Trudie Abram', '+01(321)-865-7341' );
+values (2,2, 'Trudie Abram', '+01(321)-865-7341' );
 
 insert into BCUSTOMER 
-values (3000002, 20000000003021, 'Marianna Tennyson' , '+01(727)-438-9988' );
+values (3, 3, 'Marianna Tennyson' , '+01(727)-438-9988' );
 
 insert into BCUSTOMER 
-values (3000003, 20000000003020, 'Brenden Norton', '+01(941)-3255-4312');
+values (4, 4, 'Brenden Norton', '+01(941)-3255-4312');
 
 insert into BCUSTOMER
-values (3000004, 20000000003022, 'Kyro Stephens', '+01(305)-532-8764');
+values (5, 5, 'Kyro Stephens', '+01(305)-532-8764');
 
 
 
@@ -124,19 +124,19 @@ values (3000004, 20000000003022, 'Kyro Stephens', '+01(305)-532-8764');
 
 
 insert into DRIVER
-values (100100,20000000003021, 30000000003018, 'JB9UFEWDYSBYMEUXR', 'Red', 'Tesla Inc.', 'Tesla');
+values (1,1, 500000000000010, 'JB9UFEWDYSBYMEUXR', 'Red', 'Tesla Inc.', 'Tesla');
 
  insert into DRIVER 
-values (100101, 20000000003021, 30000000003019, 'JG9SWUFRGJSGGTSBE', 'Blue', 'Daimler AG', 'Mercedes-Benz');
+values (2, 2, 500000000000011, 'JG9SWUFRGJSGGTSBE', 'Blue', 'Daimler AG', 'Mercedes-Benz');
 
 insert into DRIVER 
-values (100102, 20000000003021, 30000000003020, 'RE9HDUFKLHJAWAFRC', 'White', 'Stelantis', 'Jeep');
+values (3, 3, 500000000000012, 'RE9HDUFKLHJAWAFRC', 'White', 'Stelantis', 'Jeep');
 
 insert into DRIVER 
-values (100103, 20000000003021, 30000000003021, 'TY9JFSXMNGDKLTYSHR', 'Red', 'Hondar Motor Co.', 'Honda');
+values (4, 4, 500000000000013, 'TY9JFSXMNGDKLTYSHR', 'Red', 'Hondar Motor Co.', 'Honda');
 
 insert into DRIVER 
-values (100104, 20000000003021, 30000000003022, 'YR9HKPXHRWURWHWXS', 'Red', 'Ford Motor Co.', 'Ford');
+values (5, 5, 500000000000014, 'YR9HKPXHRWURWHWXS', 'Red', 'Ford Motor Co.', 'Ford');
 
 
 
@@ -146,15 +146,15 @@ values (100104, 20000000003021, 30000000003022, 'YR9HKPXHRWURWHWXS', 'Red', 'For
 
 --Records for SELECTION table
 insert into SELECTION
-values (400000000000100, 80000000001, 700000000, 20000000002018);
+values (1, 1, 1, 1);
 
 insert into SELECTION 
-values (400000000000101, 80000000002,  700000001, 20000000002019);
+values (2, 2,  2, 2);
 
 insert into SELECTION
-values (400000000000102, 80000000003,  700000002, 20000000002020);
+values (3, 3,  3, 3);
 
 insert into SELECTION 
-values (400000000000103, 80000000004, 700000003, 20000000002021);
+values (4, 4, 4, 4);
 insert into SELECTION 
-values (400000000000104, 80000000005, 700000004, 20000000002022);
+values (5, 5, 5, 5);
